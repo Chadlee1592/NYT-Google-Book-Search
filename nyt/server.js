53c8ab0d3cba42b4e1b7d86dbc0ab327f7675611
chad lee <chadlee@969j3f1.atp-sbs.local>
2019-03-14 09:31:07 -0700

    made changes for deployment

--- nyt/server.js
+++ nyt/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path")
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 3001;
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")))
 }
 
 // Add routes, both API and view
@@ -25,7 +27,11 @@ mongoose.connect(
   }
 );
 
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "client" , "build", "index.html"))
+})
+
 // Start the API server
-app.listen(PORT, () =>
+app.listen(process.env.PORT || 3001, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
